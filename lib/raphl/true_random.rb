55f6fc280e9ddf7286ab93e73c8115592986561b
Phillip Ridlen <phillip@ovenbits.com>
2013-09-24 21:20:26 -0500

    Use "module_function" instead of "class << self"

--- lib/raphl/true_random.rb
+++ lib/raphl/true_random.rb
@@ -1,30 +1,30 @@
 module Raphl
   module TrueRandom
-    class << self
-      # Pick a random element from collection using Random.org true random number generator
-      def select(collection = [])
-        entry_index = random(0, collection.count-1)
-        collection[entry_index]
-      end
+    module_function
 
-      def random(min, max)
-        true_random(min, max)
-      rescue SocketError, ArgumentError
-        fallback_random(min, max)
-      end
+    # Pick a random element from collection using Random.org true random number generator
+    def select(collection = [])
+      entry_index = random(0, collection.count-1)
+      collection[entry_index]
+    end
 
-      def fallback_random(min, max)
-        Random.rand(min..max)
-      end
+    def random(min, max)
+      true_random(min, max)
+    rescue SocketError, ArgumentError
+      fallback_random(min, max)
+    end
 
-      def true_random(min, max)
-        uri = "http://www.random.org/integers/?col=1&num=1&base=10&format=plain"
-        uri << "&min=#{ min }"
-        uri << "&max=#{ max }"
+    def fallback_random(min, max)
+      puts "falling back to Random#rand"
+      Random.rand(min..max)
+    end
 
-        Integer(HTTParty.get(uri))
-      end
+    def true_random(min, max)
+      uri = "http://www.random.org/integers/?col=1&num=1&base=10&format=plain"
+      uri << "&min=#{ min }"
+      uri << "&max=#{ max }"
 
+      Integer(HTTParty.get(uri))
     end
   end
 end
